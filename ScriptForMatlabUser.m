60e33071b3fe3b4f0e7877cad372e355e134f8d3
chouj <xcchris@gmail.com>
2018-10-01 23:10:26 +0800

    Update ScriptForMatlabUser.m

--- ScriptForMatlabUser.m
+++ ScriptForMatlabUser.m
@@ -22,38 +22,7 @@ end
 % read game_log.tsv into workspace
 [time,lat,lon,event,response,comment] = textread(fullfile(s,'game_log.tsv'),'%s%s%s%s%s%s','headerlines',1,'delimiter','\t');
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%% UPC blue %%%%%%%%%%%%%%%%%%%%%%%%%
-n=0;
-for i=1:length(time);
-    if strcmp(lat{i},'None')==0
-        % Pearl River Delta
-        if str2num(lat{i})>21.5&str2num(lat{i})<23.5&str2num(lon{i})>112&str2num(lon{i})<115
-            if strcmp(event{i},'captured portal')==1 % Portal Captured
-                n=n+1;
-                londata(n)=str2num(lon{i});
-                latdata(n)=str2num(lat{i});
-            end
-        end
-    end
-end
 
-[clon,ialon,iblon]=unique(londata,'stable');
-[clat,ialat,iblat]=unique(latdata,'stable');
-index=union(ialat,ialon);
-lonunique=londata(index);latunique=latdata(index); % duplicate checking
-
-% write into a kml file
-% filename = 'test_v2.kml';
-% kmlwritepoint(filename, latunique, lonunique);
-
-% coordinates transform
-[latuniqueChina,lonuniqueChina]=wgs2gcj(latunique,lonunique);
-
-f=figure;
-plot(lonuniqueChina, latuniqueChina, '.b','MarkerSize',5) ;
-hold on
-
-clear londata latdata clon ialon iblon clat ialat iblat index lonunique latunique lonuniqueChina latuniqueChina
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%% UPV green %%%%%%%%%%%%%%%%%%%%%%%%%
 n=0;
@@ -80,8 +49,9 @@ lonunique=londata(index);latunique=latdata(index); % duplicate checking
 % coordinates transform
 [latuniqueChina,lonuniqueChina]=wgs2gcj(latunique,lonunique);
 
-
-plot(lonuniqueChina, latuniqueChina, '.g','MarkerSize',4) ;
+f=figure;
+plot(lonuniqueChina, latuniqueChina, '.g','MarkerSize',9) ;
+hold on
 clear londata latdata clon ialon iblon clat ialat iblat index lonunique latunique lonuniqueChina latuniqueChina
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%% fire XMP red %%%%%%%%%%%%%%%%%%%%%%%%%
@@ -138,11 +108,68 @@ lonunique=londata(index);latunique=latdata(index); % duplicate checking
 plot(lonuniqueChina, latuniqueChina, '.c','MarkerSize',1.5) ;
 clear londata latdata clon ialon iblon clat ialat iblat index lonunique latunique lonuniqueChina latuniqueChina
 
+%%%%%%%%%%%%%%%%%%%%%%%%%%% UPC blue %%%%%%%%%%%%%%%%%%%%%%%%%
+n=0;
+for i=1:length(time);
+    if strcmp(lat{i},'None')==0
+        % Pearl River Delta
+        if str2num(lat{i})>21.5&str2num(lat{i})<23.5&str2num(lon{i})>112&str2num(lon{i})<115
+            if strcmp(event{i},'captured portal')==1 % Portal Captured
+                n=n+1;
+                londata(n)=str2num(lon{i});
+                latdata(n)=str2num(lat{i});
+            end
+        end
+    end
+end
+
+[clon,ialon,iblon]=unique(londata,'stable');
+[clat,ialat,iblat]=unique(latdata,'stable');
+index=union(ialat,ialon);
+lonunique=londata(index);latunique=latdata(index); % duplicate checking
+
+% write into a kml file
+% filename = 'test_v2.kml';
+% kmlwritepoint(filename, latunique, lonunique);
+
+% coordinates transform
+[latuniqueChina,lonuniqueChina]=wgs2gcj(latunique,lonunique);
+
+plot(lonuniqueChina, latuniqueChina, '.b','MarkerSize',10) ;
+
+clear londata latdata clon ialon iblon clat ialat iblat index lonunique latunique lonuniqueChina latuniqueChina
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%% dropped item yellow %%%%%%%%%%%%%%%%%%%%%%%%%
+n=0;
+for i=1:length(time);
+    if strcmp(lat{i},'None')==0
+        % Pearl River Delta
+        if str2num(lat{i})>21.5&str2num(lat{i})<23.5&str2num(lon{i})>112&str2num(lon{i})<115
+            if strcmp(event{i},'dropped item')==1
+                n=n+1;
+                londata(n)=str2num(lon{i});
+                latdata(n)=str2num(lat{i});
+            end
+        end
+    end
+end
+
+[clon,ialon,iblon]=unique(londata,'stable');
+[clat,ialat,iblat]=unique(latdata,'stable');
+index=union(ialat,ialon);
+lonunique=londata(index);latunique=latdata(index); % duplicate checking
+
+% coordinates transform
+[latuniqueChina,lonuniqueChina]=wgs2gcj(latunique,lonunique);
+
+
+plot(lonuniqueChina, latuniqueChina, '.y','MarkerSize',1.5) ;
+clear londata latdata clon ialon iblon clat ialat iblat index lonunique latunique lonuniqueChina latuniqueChina
+
 % add background of google map
 plot_google_map('maptype', 'roadmap');
 zoomHandle = zoom;
 set(zoomHandle, 'ActionPostCallback', @update_google_map);
 
-legend('UPC','Only UPV','Fired weapon','Login');
-
+legend('Only UPV','Fired weapon','Login','UPC','Dropped item');
 % export_fig f:\test_v1 -png -r300 -q111 -transparent
